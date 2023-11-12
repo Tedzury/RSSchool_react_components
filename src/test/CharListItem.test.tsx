fefbe737dffac4618fd59990a48f60b4423539d6
Tedzury <asdrogachev@gmail.com>
2023-11-12 13:58:53 +0300

    fix: fix test for char list item

--- src/test/CharListItem.test.tsx
+++ src/test/CharListItem.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import { userEvent } from '@testing-library/user-event';
 
@@ -14,9 +14,8 @@ describe('Chacter card component testing', () => {
     expect(listItems[3]).toHaveTextContent('Aaron Stack');
     expect(listItems[4]).toHaveTextContent('Abomination (Emil Blonsky)');
   });
-  it('After clicking on first list item, it should make new api call, fetch data and open a detailed card section for proper character', async () => {
+  it('After clicking on first list item it should open a detailed card section for proper character', async () => {
     render(<App />);
-    const request = vi.spyOn(window, 'fetch');
     expect(screen.queryByAltText('Picture of 3-D Man')).toBeNull();
     expect(screen.queryByRole('heading', { level: 2 })).toBeNull();
     expect(screen.queryByTestId('overlay')).toBeNull();
@@ -29,7 +28,15 @@ describe('Chacter card component testing', () => {
       await screen.findByRole('heading', { level: 2 })
     ).toBeInTheDocument();
     expect(await screen.findByTestId('overlay')).toBeInTheDocument();
-    expect(request).toBeCalledTimes(1);
-    expect(request.mock.calls.length === 1).toBe(true);
+  });
+  it('After clicking on first list item, it should make new api call', async () => {
+    render(<App />);
+    const request = vi.spyOn(window, 'fetch');
+    const listItems = await screen.findAllByRole('link');
+    userEvent.click(listItems[0]);
+    waitFor(() => {
+      expect(request).toBeCalledTimes(1);
+      expect(request.mock.calls.length === 1).toBe(true);
+    });
   });
 });
